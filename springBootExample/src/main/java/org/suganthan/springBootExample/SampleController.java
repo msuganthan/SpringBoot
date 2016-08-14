a1231ecda83d058a4e619890a6dd814c5859fabf
msuganthan <suganthanchandrika@gmail.com>
2016-08-14 17:26:25 +0530

    Spring Boot Sample

---
+++ springBootExample/src/main/java/org/suganthan/springBootExample/SampleController.java
@@ -0,0 +1,14 @@
+package org.suganthan.springBootExample;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+
+	@RequestMapping("/sample")
+	public String sampleIt() {
+		return "Hello! Welcome to Spring Boot Sample";
+	}
+	
+}
